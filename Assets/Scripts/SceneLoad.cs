ec962385c009107625c94ffea5affa3ea4421435
danishsshaikh <danishsshaikh@gmail.com>
2019-12-19 03:05:29 +0530

    using updated

--- Assets/Scripts/SceneLoad.cs
+++ Assets/Scripts/SceneLoad.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SceneLoad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void playGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("LevelScene");
     }
 }
