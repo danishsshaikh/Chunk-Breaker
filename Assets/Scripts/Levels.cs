1457c8837054e3fd833bff15c4953cec656824de
danishsshaikh <danishsshaikh@gmail.com>
2019-12-25 03:03:40 +0530

    Scenes

---
+++ Assets/Scripts/Levels.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Levels : MonoBehaviour
+{
+    public void lvlOne()
+    {
+        SceneManager.LoadScene("01-Level");
+    }
+
+    //public void lvlTwo()
+    //{
+    //    SceneManager.LoadScene();
+    //}
+
+    //public void lvlThree()
+    //{
+    //    SceneManager.LoadScene();
+    //}
+
+    //public void lvlFour()
+    //{
+    //    SceneManager.LoadScene();
+    //}
+
+    //public void lvlFive()
+    //{
+    //    SceneManager.LoadScene();
+    //}
+}
