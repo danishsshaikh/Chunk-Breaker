9792137a8fceaea3511e0baaaf71e72cfd33dd3d
danishsshaikh <danishsshaikh@gmail.com>
2020-03-14 01:26:00 +0530

    Update GameStatus.cs

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
 
-    [SerializeField] int pointsperBlockDestroyed = 83;
+    [SerializeField] int pointsperBlockDestroyed = 1;
 
     [SerializeField] int currentScore = 0;
 
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -19,5 +26,6 @@ public class GameStatus : MonoBehaviour
     public void AddtoScore()
     {
         currentScore += pointsperBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
