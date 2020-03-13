0b9660fbd366f28912049955b8979cda893ad434
danishsshaikh <danishsshaikh@gmail.com>
2020-03-13 23:40:11 +0530

    Update Level.cs

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] int breakableBlocks; //for debuggin
 
-    // Update is called once per frame
-    void Update()
+    public void CountBreakableBlocks()
     {
-        
+        breakableBlocks++;
     }
 }
